6789c3bf7987de8e38f0fc481fe0c9ea8cfd153a
chc-chupea <sh20240526@gmail.com>
2026-07-06 17:31:05 +0900

    Add files via upload

---
+++ templates.js
@@ -0,0 +1,146 @@
+// ==========================================
+// Miyajima Fan Studio
+// テンプレートセット定義
+// ==========================================
+
+export const TEMPLATE_SETS = {
+
+  // ========================================
+  // 一般
+  // ========================================
+  general: {
+    label: "一般",
+    note: "無地・母の日・父の日",
+
+    templates: [
+
+      {
+        id: "plain",
+        name: "無地",
+        image: "fan1-highres.png"
+      },
+
+      {
+        id: "mother",
+        name: "母の日",
+        image: "fan-mother-highres.png"
+      },
+
+      {
+        id: "father",
+        name: "父の日",
+        image: "fan-father-highres.png"
+      }
+
+    ]
+  },
+
+
+
+  // ========================================
+  // 夏
+  // ========================================
+  summer: {
+    label: "夏バージョン",
+    note: "花火・海・夏祭り",
+
+    templates: [
+
+      {
+        id: "summer01",
+        name: "花火",
+        image: "fan2026summer01-highres.png"
+      },
+
+      {
+        id: "summer02",
+        name: "海の仲間",
+        image: "fan2026summer02-highres.png"
+      },
+
+      {
+        id: "summer03",
+        name: "水彩マリン",
+        image: "fan2026summer03-highres.png"
+      },
+
+      {
+        id: "summer04",
+        name: "トロピカル",
+        image: "fan2026summer04-highres.png"
+      }
+
+    ]
+  },
+
+
+
+  // ========================================
+  // 呉ソフトボール大会
+  // ========================================
+  kure_softball: {
+    label: "呉ソフトボール大会",
+    note: "無地・大会",
+
+    templates: [
+
+      {
+        id: "plain",
+        name: "無地",
+        image: "fan1-highres.png"
+      },
+
+      {
+        id: "kure",
+        name: "呉ソフトボール大会",
+        image: "kure-softball.png"
+      }
+
+    ]
+  },
+
+
+
+  // ========================================
+  // 通販
+  // ========================================
+  ecommerce_fan: {
+    label: "通販用",
+    note: "年間販売",
+
+    templates: [
+
+      {
+        id: "plain",
+        name: "無地",
+        image: "fan1-highres.png"
+      },
+
+      {
+        id: "mother",
+        name: "母の日",
+        image: "fan-mother-highres.png"
+      },
+
+      {
+        id: "father",
+        name: "父の日",
+        image: "fan-father-highres.png"
+      },
+
+      {
+        id: "birthday",
+        name: "誕生日",
+        image: "fan-birthday-highres.png"
+      },
+
+      {
+        id: "respect",
+        name: "敬老の日",
+        image: "fan-respect-highres.png"
+      }
+
+    ]
+  }
+
+};
